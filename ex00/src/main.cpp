17f81f09b68cb137df9242c0b28fd3e004a651d8
Dimitris Chrysovergis <dchrysov@2-F-5.42heilbronn.de>
2025-07-28 11:52:01 +0200

    ex00 clean up

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,17 +1,41 @@
 #include "../inc/whatever.hpp"
 
 int main( void ) {
-	int a = 2;
-	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	{
+		int a = 2;
+		int b = 3;
+	
+		std::cout << "\nv===================== INT =======================v" << std::endl;
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::cout << "^=================================================^\n" << std::endl;
+	}
+	{
+		char a = 'a';
+		char b = 'b';
+
+		std::cout << "\nv===================== CHAR ======================v" << std::endl;
+		std::cout << "a = '" << a << "', b = '" << b << "'" << std::endl;
+		::swap(a, b);
+		std::cout << "a = '" << a << "', b = '" << b << "'" << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::cout << "^=================================================^\n" << std::endl;
+	}
+	{
+		std::string a = "chaine1";
+		std::string b = "chaine2";
+
+		std::cout << "\nv==================== STRING =====================v" << std::endl;
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::cout << "^=================================================^\n" << std::endl;
+	}
 	return 0;
 }
