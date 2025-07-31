45349fb118e671ecc61b51921b39b265fa543edd
Dimitris Chrysovergis <dchrysov@1-E-25.42heilbronn.de>
2025-07-31 14:31:36 +0200

    ex02 init

---
+++ ex02/inc/Array.hpp
@@ -0,0 +1,36 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+#include <exception>
+#include "Colors.hpp"
+
+template <typename T>
+class Array {
+	
+	private:
+		T*				_data;
+		unsigned int	_size;
+
+	public:
+		Array();
+		Array(unsigned int size);
+		Array(const Array& other);
+		~Array();
+
+		Array&			operator=(const Array& other);
+		T&				operator[](unsigned int idx);
+		static void*	operator new[](std::size_t size);
+
+		unsigned int	size(void) const; 
+
+		class IndexOutOfBounds : public std::exception {
+			public:
+				const char*	what(void) const noexcept override;
+		};
+};
+
+template <typename T>
+std::ostream&	operator<<(std::ostream& out, const Array<T>& obj);
+
+#endif
---
+++ ex02/src/Array.cpp
@@ -0,0 +1,80 @@
+#include "../inc/Array.hpp"
+
+/****************************************************
+*					CONSTRUCTORS					*
+****************************************************/
+
+template <typename T>
+Array<T>::Array() : _data(nullptr), _size(0) {
+	std::cout << "Default templated " << color("array ", YLW) << color("constructor", GRN) << " called!" << std::endl;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int size) : _data(new T[size]), _size(size) {
+	std::cout << "Parameterized templated " << color("array ", YLW) << color("constructor", GRN) << " called!" << std::endl;
+}
+
+template <typename T>
+Array<T>::Array(const Array& other) : _size(other._size){
+	other._data = new Array(_size); 
+	std::cout << "Copy templated " << color("array ", YLW) << color("constructor", GRN) << " called!" << std::endl;
+}
+
+template <typename T>
+Array<T>::~Array() {
+	delete _data;
+	std::cout << "Default " << color("template ", YLW) << color("destructor", RED) << " called!" << std::endl;
+}
+
+/****************************************************
+*					OPERATORS						*
+****************************************************/
+
+template <typename T>
+Array<T>&	Array<T>::operator=(const Array& other) {
+	if (this != &other) {
+		_size = other._size;
+		delete _data;
+		_data = new T[_size];
+		for (unsigned int i = 0; i < _size; i++) {
+			_data[i] = other._data[i];
+		}
+	}
+	return (*this);
+}
+
+template <typename T>
+void*	Array<T>::operator new[](std::size_t size) {
+	return (new Array<T>(size));
+}
+
+template <typename T>
+T&	Array<T>::operator[](unsigned int idx) {
+	return (this(idx));
+}
+
+template <typename T>
+std::ostream&	operator<<(std::ostream& out, const Array<T>& obj) {
+	for (unsigned int i = 0; i < obj.size(); i++)
+		out << obj[i] << ' ';
+	std::cout << std::endl;
+	return (out);
+}
+
+/****************************************************
+*					FUNCTIONS						*
+****************************************************/
+
+template <typename T>
+unsigned int	Array<T>::size(void) const {
+	return (_size);
+}
+
+/****************************************************
+*					EXCEPTIONS						*
+****************************************************/
+
+template <typename T>
+const char*	Array<T>::IndexOutOfBounds::what(void) const noexcept {
+	return ("Index out of bounds!\n");
+}
