d33c7b3b208c513078536c42adfa8165b17cf335
dchrysov <j.chrisov@gmail.com>
2025-08-23 12:37:37 +0100

    merged 02

--- CPP_04/ex03/src/Interfaces/ICharacter.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../inc/Character.hpp"
-
-/****************************************************
-*					CONSTRUCTORS					*
-****************************************************/
-
-ICharacter::~ICharacter() {
-	std::cout << "Character " << color("interface", YLW) << " was " << color("destroyed", RED) << "!" << std::endl;
-}
